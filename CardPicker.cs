c27983f20d6d1d081c998d8300c7be466353def4
Ian McLean <77039023+i33mclean@users.noreply.github.com>
2022-06-07 11:10:23 -0400

    Finished CardPicker

--- CardPicker.cs
+++ CardPicker.cs
@@ -8,7 +8,7 @@ namespace PickRandomCards
 {
     internal class CardPicker
     {
-        // Generates a random number 
+        // Creates a Random Number Generator called random
         static Random random = new Random();
 
         public static string[] PickSomeCards(int numberOfCards)
@@ -21,22 +21,46 @@ namespace PickRandomCards
             return pickedCards;
         }
 
-        private static string RandomSuit()
+        private static string RandomValue()
         {
-            int value = random.Next(1, 5);
+            // Get a random number from 1 to 13
+            int value = random.Next(1, 14);
 
+            // If the value is 1, return the string Ace
             if (value == 1) return "Ace";
-            if (value == 2) return "Jack";
-            if (value == 3) return "Queen";
-            if (value == 4) return "King";
+
+            // If the value is 11, return the string Jack
+            if (value == 11) return "Jack";
+
+            // If the value is 12, return the string Queen
+            if (value == 12) return "Queen";
+
+            // If the value is 13, return the string King
+            if (value == 13) return "King";
+
+            // Since the RandomValue Method is a string, value needs to be converted to a string
             return value.ToString();
         }
 
-
-        private static string RandomValue()
+        private static string RandomSuit()
         {
-            throw new NotImplementedException();
+            // Get a random number from 1 to 4
+            int value = random.Next(1, 5);
+
+            // If the value is 1, return the string Spades
+            if (value == 1) return "Spades";
+
+            // If the value is 2, return the string Hearts
+            if (value == 2) return "Hearts";
+
+            // If the value is 3, return the string Clubs
+            if (value == 3) return "Clubs";
+
+            // If it has not returned yet, return the string Diamonds
+            return "Diamonds";
         }
+
+
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,7 +11,25 @@ namespace PickRandomCards
 
         static void Main(string[] args)
         {
-        
+            Console.Write("Enter the number of cards to pick: ");
+
+            string line = Console.ReadLine();
+
+            int.TryParse(line, out int numberOfCards);
+
+            if(int.TryParse(line, out numberOfCards))
+            {
+                string [] cards = CardPicker.PickSomeCards(numberOfCards);
+
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number was not valid");
+            }
         }   
     }
 }
